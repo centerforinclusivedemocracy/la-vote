7df5abaeded7a73099befbc61b579e86d5a17c80
Kelly Armijo <karmijo@users.noreply.github.com>
2020-10-28 11:25:56 -0700

    Fix typos

--- process_data.py
+++ process_data.py
@@ -4,9 +4,9 @@ precincts_shape = r'C:\Users\karmijo\Documents\precincts_mapping\data\final_data
 registered_voters = r'C:\Users\karmijo\Documents\precincts_mapping\data\final_data\most_recent\1- Count of registered voters by precinct.csv'
 voters = r'C:\Users\karmijo\Documents\precincts_mapping\data\final_data\most_recent\2- Count of votes cast (broken out by VBM and in-person ballots) by precinct and VBM return method.csv'
 output_loc = r'C:\Users\karmijo\Documents\precincts_mapping\data\final_data\geojson_layers'
-votecenter_shape = r'C:\Users\karmijo\Documents\precincts_mapping\data\final_data\geojson_layer\vote_centers_locs.geojson'
+votecenter_shape = r'C:\Users\karmijo\Documents\precincts_mapping\data\final_data\geojson_layers\vote_centers_locs.geojson'
 votecenter_voters = r'C:\Users\karmijo\Documents\precincts_mapping\data\final_data\most_recent\3- Count of in-person ballots cast for each vote center with VBM return method_LA.csv'
-votecenter_alloc = r'C:\Users\karmijo\Documents\precincts_mapping\data\final_data\most_recent\LA Vote Center_allocations_20201027.xlsx'
+votecenter_alloc = r'C:\Users\karmijo\Documents\precincts_mapping\data\final_data\LA Vote Center_allocations_20201027.xlsx'
 
 process_precincts(precincts_shape, registered_voters, voters, output_loc, reduce_file=True, places=6)
 process_votecenters(votecenter_shape, votecenter_voters, votecenter_alloc, output_loc)
